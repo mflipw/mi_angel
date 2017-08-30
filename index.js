dd2a5681a6f7f7c1c57acadee9ec62a1f0029dee
Marvin Wiegand <mflipw@users.noreply.github.com>
2017-08-30 14:58:29 +0200

    init noble goblet console.log for angel

---
+++ index.js
@@ -0,0 +1,16 @@
+exports.noble_price = function() {
+    console.log("###############################################################################");
+    console.log("#                                                                             #");
+    console.log("#                                '._==_==_=_.'                                #");
+    console.log("#                                .-\\       /-.                                #");
+    console.log("#                               | (| noble |) |                               #");
+    console.log("#                                '-|: #1   |-'                                #");
+    console.log("#                                  \\::.    /                                  #");
+    console.log("#                                   '::. .'                                   #");
+    console.log("#                                     ) (                                     #");
+    console.log("#                                 ,__.' '.__,                                 #");
+    console.log("#                                 |  Angel  |                                 #");
+    console.log('#                                 `"""""""""`                                 #');
+    console.log("###############################################################################");
+  return true;
+};
